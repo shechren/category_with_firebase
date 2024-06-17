1591d24de83389b08a7574c04a696e2fe31db434
shechren <2182ijh@gmail.com>
2024-06-17 20:08:03 +0900

    d

--- lib/CategoryData/CategoryData.dart
+++ lib/CategoryData/CategoryData.dart
@@ -3,11 +3,7 @@ class InitCategory {
     required this.depth,
     this.id,
     this.image,
-    this.parentPrimary,
-    this.primary,
-    this.ordinary,
-    this.keyword1,
-    this.keyword2,
+    this.keyword,
   });
 
   // Category depth
@@ -16,48 +12,29 @@ class InitCategory {
   // Category id
   final String? id;
 
-  // Category parent primary
-  final int? parentPrimary;
 
-  // Category primary
-  final int? primary;
-
-  // Category ordinary
-  final int? ordinary;
-
-  // Category icons
-  // image = firebase storage url
   final String? image;
 
-  final String? keyword1;
+  final String? keyword;
 
-  final String? keyword2;
 
   // Make map from InitCategory data
   Map<String, dynamic> makeMap() {
     return {
       'depth': depth,
       'id': id ?? '',
-      'parentPrimary': parentPrimary ?? 0,
-      'primary': primary ?? 0,
-      'ordinary': ordinary ?? 0,
       'image': image ?? '',
-      'keyword1': keyword1 ?? '',
-      'keyword2': keyword2 ?? '',
+      'keyword': keyword ?? '',
     };
   }
 
   // Load map for InitCategory data
-  static InitCategory loadMap(Map<String, dynamic> doc, String id) {
+  static InitCategory loadMap(Map<String, dynamic> doc) {
     return InitCategory(
       depth: doc['depth'],
       id: doc['id'],
-      parentPrimary: doc['parentPrimary'],
-      primary: doc['primary'],
-      ordinary: doc['ordinary'],
       image: doc['image'],
-      keyword1: doc['keyword1'],
-      keyword2: doc['keyword2'],
+      keyword: doc['keyword'],
     );
   }
 }
